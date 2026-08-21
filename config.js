27b79e5285d0ed6b88706fc838292d27814af2c3
Tim Bresseleers <bresseleerstim@gmail.com>
2026-08-21 22:46:43 +0200

    improve cabine logic

--- config.js
+++ config.js
@@ -368,7 +368,7 @@ const SATURDAY_CABINE_OVERRIDES = [
         { cabineHome: "7", cabineAway: "4" }
     ]},
     { fieldName: 'B-KGV', startMinutes: 17 * 60, cabinePairs: [
-        { cabineHome: "7", cabineAway: "4" },
+        { cabineHome: "9", cabineAway: "1" },
     ]},
 
     { fieldName: 'C', startMinutes: 10 * 60, cabinePairs: [
